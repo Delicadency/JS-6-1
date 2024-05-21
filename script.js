1321be27820c07c1ee24c7ef88615b9657f9cb4a
Agata Kamińska <a.fluffypie@gmail.com>
2024-05-21 12:48:33 +0200

    added createDOM function

---
+++ script.js
@@ -0,0 +1,32 @@
+/* Stwórz następujące funkcje:
+
+createDOM
+
+input: (String) Tag Name (np. "div")
+output: Tag (np. <div class="item">)
+zadanie: stwórz element, dodaj do niego klasę item, i zwróć z funkcji
+
+removeDOM
+
+input: (String) CSS Selector (np. "div")
+output: Tag (np. <div>)
+zadanie: znajdź przekazany element, usuń go z DOM, i zwróć usunięty element z funkcji
+
+
+Stwórz pliki index.js (umieść w nim powyższe funkcje) oraz index.html (gdzie umieścisz wymagane elementy DOM). */
+
+function createDOM(tagName) {
+  if (typeof tagName !== "string") {
+    console.error("Wprowadź prawidłową nazwę tagu, który chcesz utworzyć.");
+    return null;
+  }
+
+  const tag = document.createElement(tagName);
+  tag.classList.add("item");
+  const body = document.querySelector("body");
+  body.insertAdjacentElement("afterbegin", tag);
+
+  document.addEventListener("DOMContentLoaded", () => {
+    createDOM(tagName);
+  });
+}
