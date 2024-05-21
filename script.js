7aa93b58f4bba0fcda50dda9a99442472c1de45b
Agata Kamińska <a.fluffypie@gmail.com>
2024-05-21 13:19:14 +0200

    removed comments

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-/* Stwórz następujące funkcje:
-
-createDOM
-
-input: (String) Tag Name (np. "div")
-output: Tag (np. <div class="item">)
-zadanie: stwórz element, dodaj do niego klasę item, i zwróć z funkcji
-
-removeDOM
-
-input: (String) CSS Selector (np. "div")
-output: Tag (np. <div>)
-zadanie: znajdź przekazany element, usuń go z DOM, i zwróć usunięty element z funkcji
-
-
-Stwórz pliki index.js (umieść w nim powyższe funkcje) oraz index.html (gdzie umieścisz wymagane elementy DOM). */
-
 function createDOM(tagName) {
   if (typeof tagName !== "string") {
     console.error("Wprowadź prawidłową nazwę tagu, który chcesz utworzyć.");
